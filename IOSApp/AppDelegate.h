3d5f93fba2edbadef4ccb2433d55b3d843d121b4
林 <lin@lindemac-mini.local>
2016-10-10 14:54:35 +0800

    Initial Commit

---
+++ IOSApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  IOSApp
+//
+//  Created by 林 on 2016/10/10.
+//  Copyright © 2016年 林com.xxx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
